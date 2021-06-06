2fca0a2b2e086edc7f30cfa1a50e08cc57346ba4
Dak <vectorskeleton@gmail.com>
2021-06-06 05:02:45 -0700

    Create GiantCharger.cs

---
+++ Twitch/Events/D100/GiantCharger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchDice.Twitch.Events.D100
+{
+    public class GiantCharger : DiceEvent<NoNetworkData>
+    {
+        public override bool RequireNetworking => false;
+
+        public override bool HasNetworkData => false;
+
+        public override string EventName => "Unkillable Giant Charger";
+
+        public override string EventId => "d100_spawnGiantCharger";
+
+        public override DiceTier Tier => DiceTier.D100;
+
+        public override bool CanBeTriggered()
+        {
+            return true;
+        }
+
+        protected override void TriggerClient(NoNetworkData NetworkInfo)
+        {
+            
+        }
+
+        protected override NoNetworkData TriggerHost()
+        {
+
+            return new NoNetworkData();
+        }
+    }
+}
