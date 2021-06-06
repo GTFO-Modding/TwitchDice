9882cbb53e6b6616802f42874d6bf4917fa5c065
Dak <vectorskeleton@gmail.com>
2021-06-06 05:45:32 -0700

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchDice
+{
+    public static class Config
+    {
+        public static uint GIANT_CHARGER_ID = 39U;
+    }
+}
