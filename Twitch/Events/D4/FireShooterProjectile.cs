5b9d64c051964ffef69d786c898a91c0554518c9
Dak <vectorskeleton@gmail.com>
2021-06-09 01:54:35 -0700

    Create FireShooterProjectile.cs

---
+++ Twitch/Events/D4/FireShooterProjectile.cs
@@ -0,0 +1,42 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchDice.Util;
+using UnityEngine;
+
+namespace TwitchDice.Twitch.Events.D4
+{
+    class FireShooterProjectile : DiceEvent<NoNetworkData>
+    {
+        public override bool RequireNetworking => false;
+
+        public override bool HasNetworkData => false;
+
+        public override string EventName => "Shooter Projectile";
+
+        public override string EventId => "d4_shooterProj";
+
+        public override DiceTier Tier => DiceTier.D4;
+
+        public override bool CanBeTriggered()
+        {
+            return PlayerUtil.PlayerCount > 1;
+        }
+
+        protected override void TriggerClient(NoNetworkData NetworkInfo)
+        {
+            
+        }
+
+        protected override NoNetworkData TriggerHost()
+        {
+            var localPlayer = PlayerManager.GetLocalPlayerAgent();
+            if (PlayerUtil.TryGetRandomPlayerAgent(out PlayerAgent target, false))
+            {
+                ProjectileManager.WantToFireTargeting(ProjectileType.TargetingSmall, target, localPlayer.CamPos, localPlayer.Rotation * Vector3.forward, 1, 0f);
+            }
+            return new NoNetworkData();
+        }
+    }
+}
