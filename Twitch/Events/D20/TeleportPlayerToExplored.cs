61c0b00ad62b75c6ed14559772c58c943fcbdaa3
Dak <vectorskeleton@gmail.com>
2021-09-24 01:53:06 -0700

    Teleport 2 somewhere explored (breaks for some reason after a few uses)

---
+++ Twitch/Events/D20/TeleportPlayerToExplored.cs
@@ -0,0 +1,39 @@
+﻿using AIGraph;
+using Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchDice.Utilities;
+using UnityEngine;
+
+namespace TwitchDice.Twitch.Events.D20
+{
+    public class TeleportPlayerToExplored : DiceEvent<TPE>
+    {
+        public override string EventName => "Woosh!";
+
+        public override string EventID => "tpe";
+
+        protected override DiceTier DiceTier => DiceTier.D20;
+
+        public override void ReceiveClient(ulong sender, TPE packet)
+        {
+            TpToRandomExploredLocation();
+        }
+
+        public override void TriggerHost()
+        {
+            TpToRandomExploredLocation();
+            TriggerClient();
+        }
+
+        private void TpToRandomExploredLocation()
+        {
+            AIG_CourseNode chosenNode = NodeUtil.GetReachableNodes(PlayerUtil.LocalPlayerAgent.CourseNode, 100).GetRandomElement<AIG_CourseNode>();
+            Vector3 randomPosInNode = chosenNode.GetRandomPositionInside();
+            PlayerUtil.LocalPlayerAgent.PlayerCharacterController.ManualMoveTo(randomPosInNode);
+        }
+    }
+
+    public struct TPE { }
+}
