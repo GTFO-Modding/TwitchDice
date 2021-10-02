725e0a6e4e6d91c4adafeb1810b19b00a34b7360
Dak <vectorskeleton@gmail.com>
2021-10-02 15:26:15 -0700

    Create ExampleEvent.cs

---
+++ Twitch/Events/ExampleEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchDice.Twitch.Events.D3
+{
+    public class ExampleEvent : DiceEvent<ExampleData>
+    {
+        public override string EventName => "Example";
+
+        public override string EventID => "example";
+
+        protected override DiceTier DiceTier => DiceTier.D20;
+
+        public override void ReceiveClient(ulong sender, ExampleData packet)
+        {
+            
+        }
+
+        public override void TriggerHost()
+        {
+
+            TriggerClient(new ExampleData());
+        }
+    }
+
+    public struct ExampleData
+    {
+
+    }
+}
