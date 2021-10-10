d41659e65c63d9dbe760758174f072dc56d9d52c
Dak <vectorskeleton@gmail.com>
2021-10-09 17:10:58 -0700

    Comment out the example event

--- Twitch/Events/ExampleEvent.cs
+++ Twitch/Events/ExampleEvent.cs
@@ -4,28 +4,28 @@ using System.Text;
 
 namespace TwitchDice.Twitch.Events.D3
 {
-    public class ExampleEvent : DiceEvent<ExampleData>
-    {
-        public override string EventName => "Example";
-
-        public override string EventID => "example";
-
-        protected override DiceTier DiceTier => DiceTier.D20;
-
-        public override void ReceiveClient(ulong sender, ExampleData packet)
-        {
-            
-        }
-
-        public override void TriggerHost()
-        {
-
-            TriggerClient(new ExampleData());
-        }
-    }
-
-    public struct ExampleData
-    {
-
-    }
+    //public class ExampleEvent : DiceEvent<ExampleData>
+    //{
+    //    public override string EventName => "Example";
+    //
+    //    public override string EventID => "example";
+    //
+    //    protected override DiceTier DiceTier => DiceTier.D20;
+    //
+    //    public override void ReceiveClient(ulong sender, ExampleData packet)
+    //    {
+    //        
+    //    }
+    //
+    //    public override void TriggerHost()
+    //    {
+    //
+    //        TriggerClient(new ExampleData());
+    //    }
+    //}
+    //
+    //public struct ExampleData
+    //{
+    //
+    //}
 }
