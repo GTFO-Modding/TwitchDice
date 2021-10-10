de485874c15cf0275dfded35b62351a217ac4b4a
Dak <vectorskeleton@gmail.com>
2021-10-10 14:52:30 -0700

    Update GiveRandomResource.cs

--- Twitch/Events/D4/GiveRandomResource.cs
+++ Twitch/Events/D4/GiveRandomResource.cs
@@ -1,4 +1,5 @@
-﻿using Player;
+﻿using AK;
+using Player;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,7 +7,7 @@ using TwitchDice.Utilities;
 
 namespace TwitchDice.Twitch.Events.D4
 {
-    public class GiveRandomResource : DiceEvent
+    public class GiveRandomResource : DiceEvent<GiveRanRes>
     {
         public override string EventName => "Random Resource";
 
@@ -14,25 +15,52 @@ namespace TwitchDice.Twitch.Events.D4
 
         protected override DiceTier DiceTier => DiceTier.D4;
 
+        public override void ReceiveClient(ulong sender, GiveRanRes packet)
+        {
+            GiveResource(PlayerUtil.LocalPlayerAgent);
+        }
+
         public override void TriggerHost()
         {
             PlayerUtil.TryGetRandomPlayerAgent(out PlayerAgent player);
-            int res = Main.rnd.Next(0, 3);
+            if (player.IsLocallyOwned)
+            {
+                GiveResource(player);
+            } else
+            {
+                TriggerClient(new GiveRanRes(), player.Owner);
+            }
+        }
+
+        private void GiveResource(PlayerAgent player)
+        {
+            int res = Main.rnd.Next(0, 4);
             switch (res)
             {
                 case 0:
                     player.GiveHealth(0.2f);
+                    player.Sound.Post(EVENTS.MEDPACK_APPLY);
                     break;
                 case 1:
                     player.GiveAmmoRel(0.2f, 0.2f, 0.0f);
+                    player.Sound.Post(EVENTS.FOLEY_USE_AMMO_PACK_FINISHED);
+                    player.Sound.Post(EVENTS.AMMOPACK_APPLY);
                     break;
                 case 2:
                     player.GiveAmmoRel(0, 0, 0.2f);
+                    player.Sound.Post(EVENTS.FOLEY_USE_AMMO_PACK_FINISHED);
+                    player.Sound.Post(EVENTS.AMMOPACK_APPLY);
                     break;
                 case 3:
                     player.GiveDisinfection(0.2f);
+                    player.Sound.Post(EVENTS.DISINFECTION_SPRAY_ON_VISOR);
                     break;
             }
         }
+    }
+
+    public struct GiveRanRes
+    {
+
     }
 }
