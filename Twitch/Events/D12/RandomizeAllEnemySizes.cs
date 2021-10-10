7a1d027f2d200131857d6799f20748d99b9274f3
Panthr75 <jjireland@stu.naperville203.org>
2021-10-10 17:39:10 -0500

    Randomize All Enemy Sizes Event

---
+++ Twitch/Events/D12/RandomizeAllEnemySizes.cs
@@ -0,0 +1,46 @@
+﻿
+
+using Enemies;
+using System;
+using UnityEngine;
+
+namespace TwitchDice.Twitch.Events.D12
+{
+    public class RandomizeAllEnemySizes : DiceEvent<RAES>
+    {
+        public override string EventName => "Randomize All Enemy Size";
+
+        public override string EventID => "randomizeSizes";
+
+        protected override DiceTier DiceTier => DiceTier.D12;
+
+        public override void ReceiveClient(ulong sender, RAES packet)
+        {
+            int randomSeed = Main.rnd.Next(int.MinValue, int.MaxValue);
+
+            var random = new System.Random(randomSeed);
+
+            foreach (var enemy in GameObject.FindObjectsOfType<EnemyAgent>())
+            {
+                enemy.transform.localScale = enemy.transform.localScale * (random.Next(20, 150) / 100f);
+            }
+        }
+
+        public override void TriggerHost()
+        {
+            int randomSeed = Main.rnd.Next(int.MinValue, int.MaxValue);
+
+            var random = new System.Random(randomSeed);
+
+            foreach (var enemy in GameObject.FindObjectsOfType<EnemyAgent>())
+            {
+                enemy.transform.localScale = enemy.transform.localScale * (random.Next(20, 150) / 100f);
+            }
+
+            this.TriggerClient();
+        }
+    }
+
+    public struct RAES
+    { }
+}
