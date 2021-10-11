8ee69a32eaa53325fb5cbf1e2aba4760d8d7c274
Dak <vectorskeleton@gmail.com>
2021-10-10 23:09:42 -0700

    Create 180Player.cs

---
+++ Twitch/Events/D4/180Player.cs
@@ -0,0 +1,36 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchDice.Utilities;
+
+namespace TwitchDice.Twitch.Events.D4
+{
+    public class Player180 : DiceEvent<P80>
+    {
+        public override string EventName => "go back";
+
+        public override string EventID => "180";
+
+        protected override DiceTier DiceTier => DiceTier.D6;
+
+        public override void ReceiveClient(ulong sender, P80 packet)
+        {
+            PlayerUtil.LocalPlayerAgent.FPSCamera.m_yaw += 180;
+        }
+
+        public override void TriggerHost()
+        {
+            PlayerUtil.TryGetRandomPlayerAgent(out PlayerAgent target);
+            if (target.IsLocallyOwned)
+                target.FPSCamera.m_yaw += 180;
+            else
+                TriggerClient(new P80(), target.Owner);
+        }
+    }
+
+    public struct P80
+    {
+
+    }
+}
