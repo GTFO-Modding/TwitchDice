94a776c1e1202378afd8e1a48db7cd75e24b25e7
Chase Tuggle <chasetug@gmail.com>
2021-10-16 02:21:12 -0500

    change staticseed on rebuildlevel

--- Twitch/Events/D100/RebuildLevel.cs
+++ Twitch/Events/D100/RebuildLevel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Runtime.InteropServices;
+using GameData;
+using Globals;
 using LevelGeneration;
 using TwitchDice.Utilities;
 using UnityEngine;
@@ -16,26 +19,40 @@ namespace TwitchDice.Twitch.Events.D100
 
         public override void ReceiveClient(ulong sender, RL packet)
         {
-            TimedEvents.Start(this.DoTriggerEvent());
+            TimedEvents.Start(this.DoTriggerEvent(packet.seed));
         }
         
         public override void TriggerHost()
         {
-            this.TriggerClient(new RL());
-            TimedEvents.Start(this.DoTriggerEvent());
+            int seed = (int) (Main.rnd.NextDouble() * Int32.MaxValue);
+            this.TriggerClient(new RL(seed));
+            TimedEvents.Start(this.DoTriggerEvent(seed));
         }
         
-        private IEnumerator DoTriggerEvent()
+        private IEnumerator DoTriggerEvent(int seed)
         {   
+            Log.Warning($"{seed}");
             Builder.Current.OnLevelCleanup();
+
+            Global.UseStaticSeed = true;
+            Global.StaticSeed = seed;
+            
             yield return new WaitForEndOfFrame();
+            
             Builder.Current.Build();
+            
+            Global.UseStaticSeed = false;
         }
     }
     
     [StructLayout(LayoutKind.Sequential)]
     public struct RL
     {
+        public int seed;
         
+        public RL(int seed)
+        {
+            this.seed = seed;
+        }
     }
 }
