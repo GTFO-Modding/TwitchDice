7752f2c16489777aa0c4fec4117319605b13e9b1
Dak <vectorskeleton@gmail.com>
2021-10-15 19:40:57 -0700

    Create PlaceMine.cs

---
+++ Twitch/Events/D6/PlaceMine.cs
@@ -0,0 +1,33 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchDice.Utilities;
+using UnityEngine;
+
+namespace TwitchDice.Twitch.Events.D6
+{
+    public class PlaceMine : DiceEvent
+    {
+        public override string EventName => "Materialize Mine";
+
+        public override string EventID => "mineLook";
+
+        protected override DiceTier DiceTier => DiceTier.D6;
+
+        public override void TriggerHost()
+        {
+            PlayerUtil.TryGetRandomPlayerAgent(out PlayerAgent target);
+            var mine = new pItemData
+            {
+                itemID_gearCRC = 125U
+            };
+
+            Ray ray = new Ray(target.EyePosition, target.TargetLookDir);
+            Physics.Raycast(ray, out RaycastHit hit, 100000, LayerManager.MASK_CAMERA_RAY);
+            Vector3 direction = (hit.point - target.EyePosition).normalized;
+            var rot = Quaternion.LookRotation(direction, Vector3.up);
+            ItemReplicationManager.SpawnItem(mine, null, ItemMode.Instance, hit.point, rot, target.CourseNode, target);
+        }
+    }
+}
