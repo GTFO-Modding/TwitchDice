25b5655f227d57700143d2a3c74d08886fae5657
Dak <vectorskeleton@gmail.com>
2021-10-17 03:51:30 -0700

    Create GlowstickVomit.cs

---
+++ Twitch/Events/D3/GlowstickVomit.cs
@@ -0,0 +1,47 @@
+﻿using GameData;
+using Player;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TwitchDice.Utilities;
+using UnityEngine;
+
+namespace TwitchDice.Twitch.Events.D3
+{
+    public class GlowstickVomit : DiceEvent
+    {
+        public override string EventName => "Glowstick Vomit";
+
+        public override string EventID => "glowV";
+
+        protected override DiceTier DiceTier => DiceTier.D3;
+
+        public override int Time => 5;
+
+        const int SpawnCount = 20;
+        const int ThrowForce = 10;
+
+        public override void TriggerHost()
+        {
+            PlayerUtil.TryGetRandomPlayerAgent(out PlayerAgent target);
+            TimedEvents.StartTimedEvent(Vomit(target), this);
+        }
+
+        private IEnumerator Vomit(PlayerAgent target)
+        {
+            pItemData data = default;
+            data.itemID_gearCRC = 114U;
+
+            for (int i = 0; i < SpawnCount; i++)
+            {
+                var hit = SpawnUtil.GetRandomPointAround(target.EyePosition);
+                SpawnUtil.ThrowConsumable(target.EyePosition, hit.point, target, data, ThrowForce);
+                float delay = (float)Time / (float)SpawnCount;
+                yield return new WaitForSecondsRealtime(delay);
+            }
+
+            yield break;
+        }
+    }
+}
