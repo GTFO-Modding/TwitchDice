c63fecac9add64ef7c730ff387ac6e41f504b452
Dak <vectorskeleton@gmail.com>
2021-10-23 19:15:11 -0700

    Disable snow man event

--- Twitch/Events/D100/SnowFriend.cs
+++ Twitch/Events/D100/SnowFriend.cs
@@ -9,40 +9,40 @@ using UnityEngine;
 
 namespace TwitchDice.Twitch.Events.D100
 {
-    public class SnowFriend : DiceEvent<sFriend>
-    {
-        public override string EventName => "a friend :)";
-
-        public override string EventID => "friend";
-
-        protected override DiceTier DiceTier => DiceTier.D100;
-
-        static bool Triggered = false;
-
-        public override bool CanBeTriggered()
-        {
-            return !Triggered;
-        }
-
-        public override void ReceiveClient(ulong sender, sFriend packet)
-        {
-            TriggerCommon();
-        }
-
-        public override void TriggerHost()
-        {
-            Triggered = true;
-            TriggerCommon();
-            TriggerClient();
-        }
-
-        private void TriggerCommon()
-        {
-            GameObject snowman = UnityEngine.Object.Instantiate(AssetAPI.GetLoadedAsset("ASSETS/SNOWMAN.PREFAB")?.TryCast<GameObject>());
-            snowman.transform.position = PlayerUtil.LocalPlayerAgent.Position;
-            snowman.AddComponent<SnowmanAI>();
-        }
-    }
+    //public class SnowFriend : DiceEvent<sFriend>
+    //{
+    //    public override string EventName => "a friend :)";
+    //
+    //    public override string EventID => "friend";
+    //
+    //    protected override DiceTier DiceTier => DiceTier.D100;
+    //
+    //    static bool Triggered = false;
+    //
+    //    public override bool CanBeTriggered()
+    //    {
+    //        return !Triggered;
+    //    }
+    //
+    //    public override void ReceiveClient(ulong sender, sFriend packet)
+    //    {
+    //        TriggerCommon();
+    //    }
+    //
+    //    public override void TriggerHost()
+    //    {
+    //        Triggered = true;
+    //        TriggerCommon();
+    //        TriggerClient();
+    //    }
+    //
+    //    private void TriggerCommon()
+    //    {
+    //        GameObject snowman = UnityEngine.Object.Instantiate(AssetAPI.GetLoadedAsset("ASSETS/SNOWMAN.PREFAB")?.TryCast<GameObject>());
+    //        snowman.transform.position = PlayerUtil.LocalPlayerAgent.Position;
+    //        snowman.AddComponent<SnowmanAI>();
+    //    }
+    //}
 
     public struct sFriend
     {
