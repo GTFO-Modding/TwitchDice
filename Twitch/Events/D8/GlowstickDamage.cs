fd41453672867992fe06cce112bead2900d4b1d2
Dak <vectorskeleton@gmail.com>
2021-10-30 01:30:18 -0700

    Create GlowstickDamage.cs

---
+++ Twitch/Events/D8/GlowstickDamage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchDice.Twitch.Events.D8
+{
+    //public class GlowstickDamage : DiceEvent
+    //{
+    //    public override string EventName => "Painsticks";
+    //
+    //    public override string EventID => "painsticks";
+    //
+    //    protected override DiceTier DiceTier => DiceTier.D8;
+    //
+    //    public override int Time => 30;
+    //
+    //    public override bool CanBeTriggered()
+    //    {
+    //        return base.CanBeTriggered();
+    //    }
+    //
+    //    public override void TriggerHost()
+    //    {
+    //        
+    //    }
+    //}
+}
