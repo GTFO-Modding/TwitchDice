c6e3738f9d57a240e8c0fddbc81d4357de2aee32
Dak <vectorskeleton@gmail.com>
2021-11-03 02:56:02 -0700

    https://discord.com/channels/782438773690597389/831651015388037171/905393618188111902

--- Twitch/DiceEvent.cs
+++ Twitch/DiceEvent.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Newtonsoft.Json;
-using Player;
-using SNetwork;
-using TwitchLib.Client.Models;
-using Nidhogg.Managers;
-using BepInEx.Configuration;
+﻿using BepInEx.Configuration;
 using GTFO.API;
+using SNetwork;
+using System.Collections.Generic;
 
 namespace TwitchDice.Twitch
 {
@@ -87,6 +81,7 @@ namespace TwitchDice.Twitch
         /// Used internally to register this event for networking (if required)
         /// </summary>
         public virtual void Register() { }
+
         /// <summary>
         /// Code that runs on the hosts end when this event is activated
         /// </summary>
@@ -101,13 +96,15 @@ namespace TwitchDice.Twitch
     public abstract class DiceEvent<T> : DiceEvent where T : struct
     {
         bool _registered = false;
+        string _networkEvent;
 
         public override void Register()
         {
             if (_registered) return;
-            NetworkAPI.RegisterEvent<T>($"TwitchDice_DiceEvent_{typeof(T).Name}", ReceiveClient);
+            _networkEvent = $"TwitchDice_DiceEvent_{typeof(T).Name}";
+            NetworkAPI.RegisterEvent<T>(_networkEvent, ReceiveClient);
             _registered = true;
-            Log.Debug($"Registered {EventName} with ID {EventID}");
+            Log.Debug($"Registered {EventName} with ID {EventID} and network name of {_networkEvent}");
         }
 
         /// <summary>
@@ -124,7 +121,7 @@ namespace TwitchDice.Twitch
         /// <param name="packet">The packet to send</param>
         protected void TriggerClient(T packet)
         {
-            NetworkAPI.InvokeEvent($"TwitchDice_DiceEvent_{typeof(T).Name}", packet);
+            NetworkAPI.InvokeEvent(_networkEvent, packet);
         }
 
         /// <summary>
@@ -134,7 +131,7 @@ namespace TwitchDice.Twitch
         /// <param name="target">The target player</param>
         protected void TriggerClient(T packet, SNet_Player target)
         {
-            NetworkAPI.InvokeEvent($"TwitchDice_DiceEvent_{typeof(T).Name}", packet, target);
+            NetworkAPI.InvokeEvent(_networkEvent, packet, target);
         }
 
         /// <summary>
@@ -144,7 +141,7 @@ namespace TwitchDice.Twitch
         /// <param name="players">A list of players to send to</param>
         protected void TriggerClient(T packet, List<SNet_Player> players)
         {
-            NetworkAPI.InvokeEvent($"TwitchDice_DiceEvent_{typeof(T).Name}", packet, players);
+            NetworkAPI.InvokeEvent(_networkEvent, packet, players);
         }
 
         /// <summary>
@@ -154,130 +151,4 @@ namespace TwitchDice.Twitch
         /// <param name="packet">The recieved packet</param>
         public abstract void ReceiveClient(ulong sender, T packet);
     }
-
-    //OLD
-
-    /*
-    public abstract class OldDiceEvent<T> : IDiceEvent where T : struct
-    {
-        public abstract bool RequireNetworking { get; }
-        public abstract bool HasNetworkData { get; }
-        /// <summary>
-        /// {User} rolled a {DiceTier} :: {EventName}
-        /// </summary>
-        public abstract string EventName { get; }
-        public abstract string EventId { get; }
-        public abstract DiceTier Tier { get; }
-        protected bool IsHost
-        {
-            get
-            {
-                if (SNet.Core.TryGetLobbyOwner(out SNet_Player player))
-                {
-                    return player.IsLocal;
-                }
-                Log.Error("Couldn't get lobby host :(");
-                return false;
-            }
-        }
-
-        public PlayerAgent LocalPlayer
-        {
-            get
-            {
-                return PlayerManager.GetLocalPlayerAgent();
-            }
-        }
-
-        protected NoNetworkData NoNetworkData = new NoNetworkData();
-        protected NetworkedNoData NetworkedNoData = new NetworkedNoData();
-
-
-        public string Activate(EventInfo eventInfo)
-        {
-            if (eventInfo.EventNetworkData != "" && RequireNetworking && HasNetworkData)
-            {
-                try
-                {
-                    var incomingNetworkData = JsonConvert.DeserializeObject<T>(eventInfo.EventNetworkData);
-                    TriggerClient(incomingNetworkData);
-                    return null;
-                } catch(Exception e) { Log.Error($"-- Issue while trying to handle networked event! D: -- \n{e}"); }
-            }
-
-            T outgoingNetworkData = TriggerHost();
-
-            if (RequireNetworking && HasNetworkData)
-            {
-                return JsonConvert.SerializeObject(outgoingNetworkData);
-            }
-
-            return null;
-        }
-        protected abstract T TriggerHost();
-
-        protected abstract void TriggerClient(T NetworkInfo);
-
-        public abstract bool CanBeTriggered();
-
-        public override string ToString()
-        {
-            return $"RequireNetworking: {RequireNetworking}\nEventName: {EventName}\nEventId:{EventId}\nTier:{Tier}";
-        }
-
-        public DiceTier GetDicetier()
-        {
-            return Tier;
-        }
-        public string GetEventName()
-        {
-            return EventName;
-        }
-        public string GetId()
-        {
-            return EventId;
-        }
-        public bool GetRequireNetworking()
-        {
-            return RequireNetworking;
-        }
-    }
-
-    //public interface IDiceEvent
-    //{
-    //    public string Activate(EventInfo eventInfo);
-    //    public DiceTier GetDicetier();
-    //    public string GetEventName();
-    //    public string GetId();
-    //    public bool CanBeTriggered();
-    //    public bool GetRequireNetworking();
-    //}
-
-    public struct EventInfo
-    {
-        public string EventNetworkData;
-        public string ActivatorUsername;
-        public DiceTier Tier;
-    }
-
-    public struct TargetPlayer
-    {
-        public int TargetedPlayer;
-
-        public TargetPlayer(PlayerAgent target)
-        {
-            TargetedPlayer = target.PlayerSlotIndex;
-        }
-    }
-
-    public struct NetworkedNoData
-    {
-
-    }
-
-    public struct NoNetworkData
-    {
-
-    }
-    */
 }
