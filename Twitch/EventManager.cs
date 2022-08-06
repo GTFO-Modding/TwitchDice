7eb784a5deb36b090d08730dcb1fdc37d1f23df1
Dak <vectorskeleton@gmail.com>
2022-08-05 23:38:10 -0700

    Null checks

--- Twitch/EventManager.cs
+++ Twitch/EventManager.cs
@@ -26,7 +26,7 @@ namespace TwitchDice.Twitch
             Log.Debug("Created events dictionary");
             foreach (Type item in types)
             {
-                string diceEventID = null;
+                string? diceEventID = null;
                 try
                 {
                     if (item.GetConstructors().Length == 0)
@@ -34,11 +34,11 @@ namespace TwitchDice.Twitch
                         Log.Error("No constructor????");
                     }
 
-                    object diceType = Activator.CreateInstance(item);
+                    object? diceType = Activator.CreateInstance(item);
 
                     if (diceType == null) { Log.Warning("Unable to instantiate type"); return; }
 
-                    IDiceEvent diceEvent = diceType as IDiceEvent;
+                    if (diceType is not IDiceEvent diceEvent) { Log.Error("Tried to register an invalid dice event!"); continue; }
                     diceEventID = diceEvent.EventID;
 
                     if (!diceEvent.Enabled)
@@ -65,10 +65,10 @@ namespace TwitchDice.Twitch
             this.TierLookup = (Lookup<DiceTier, IDiceEvent>)this.Events.ToLookup(p => p.Value.Tier, p => p.Value);
         }
 
-        public bool TryActivateEvent(string eventID, string activator = null)
+        public bool TryActivateEvent(string eventID, string? activator = null)
         {
             
-            if (!this.Events.TryGetValue(eventID, out IDiceEvent diceEvent))
+            if (!this.Events.TryGetValue(eventID, out IDiceEvent? diceEvent))
             {
                 return false;
             }
@@ -83,7 +83,7 @@ namespace TwitchDice.Twitch
             return true;
         }
 
-        public bool TryActivateEventOfTier(DiceTier tier, string activator = null)
+        public bool TryActivateEventOfTier(DiceTier tier, string? activator = null)
         {
             IEnumerable<IDiceEvent> events = this.TierLookup[tier];
             events = events.Where(e => e.CanBeTriggered());
@@ -98,7 +98,7 @@ namespace TwitchDice.Twitch
             return true;
         }
 
-        private static void ActivateEvent(IDiceEvent diceEvent, string activator = null)
+        private static void ActivateEvent(IDiceEvent diceEvent, string? activator = null)
         {
             if (activator == null)
             {
@@ -117,71 +117,5 @@ namespace TwitchDice.Twitch
                 Log.Error(e);
             }
         }
-
-
-
-        //struct DEvent
-        //{
-        //    DiceTier Tier;
-        //    IDiceEvent Event;
-        //}
-
-
-        //public static bool TryActivateEvent(string eventID, string networkData)
-        //{
-        //    Log.Debug($"Trying to activate event with ID: {eventID}");
-        //    IDiceEvent diceEvent = EventList.Events.Find(diceEvent => { return diceEvent.GetId() == eventID; });
-        //    if (diceEvent == null) return false;
-        //    try
-        //    {
-        //        if (!ActivateEvent(diceEvent, new EventInfo() { EventNetworkData = networkData, ActivatorUsername = "DEBUG", Tier = diceEvent.GetDicetier() }))
-        //            Log.Warning($"Trigger requirements not met for ID {eventID}");
-        //        else return true;
-        //        return false;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Log.Warning("Exception while trying to activate event!");
-        //        Log.Error(e);
-        //        return false;
-        //    }
-        //}
-        //public static bool TryActivateEventOfTier(EventInfo eventInfo)
-        //{
-        //    try
-        //    {
-        //        var possibleEvents = EventList.Events.FindAll(dice => { return dice.GetDicetier() == eventInfo.Tier; });
-        //        if (possibleEvents.Count == 0) return false;
-        //
-        //        var diceEvent = possibleEvents.GetRandomElement<IDiceEvent>();
-        //
-        //        return ActivateEvent(diceEvent, eventInfo);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Log.Error(e);
-        //    }
-        //    return false;
-        //}
-        //
-        //private static bool ActivateEvent(IDiceEvent diceEvent, EventInfo eventInfo)
-        //{
-        //    Log.Debug($"-- Triggering Event --\n{diceEvent}");
-        //
-        //    if (!diceEvent.CanBeTriggered()) return false;
-        //
-        //    string networkInfo = diceEvent.Activate(eventInfo);
-        //    if (diceEvent.GetRequireNetworking() && PlayerUtil.IsHost)
-        //    {
-        //        Log.Debug($"Updating Lobby Data with event {diceEvent.GetEventName()}");
-        //        var lobby = new CSteamID(SNet.Lobby.Identifier.ID);
-        //
-        //        SteamMatchmaking.SetLobbyData(lobby, Main.EVENT_ID_KEY, diceEvent.GetId());
-        //        SteamMatchmaking.SetLobbyData(lobby, Main.EVENT_INFO_KEY, networkInfo);
-        //    }
-        //
-        //    ChatUtil.EventSpeak(eventInfo, diceEvent.GetEventName());
-        //    return true;
-        //}
     }
 }
